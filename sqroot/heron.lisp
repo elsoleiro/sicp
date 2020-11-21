f4a0325db45b8152b3465b02eb7f564fe5f32bff
elsoleiro <elsoleiro@gmail.com>
2020-11-21 17:03:22 +0000

    sqroot

---
+++ sqroot/heron.lisp
@@ -0,0 +1,29 @@
+(defun average (x y) ; formal params two integers
+    (/ (+ x y) 2)) ; quotient of the sum of those two integers and 2
+
+(defun square (x) ; formal parameter of an integer
+    (* x x)) ; return the product of that integer
+
+(defun absolute (x) ; formal param of an integer
+    (if (< x 0) ; if this condition is true
+        (- x) ; negate x
+        x)) ; else, return x
+
+(defun improve (guess x) ; two formal parameters, both integers
+    (average guess (/ x guess))) ; invoke average on guess and the quotient of x and guess
+
+(defvar tolerance .001) ; alias for precision, or, what is "good enough"
+(defun  tolerable(guess x) ; formal parameter of two integers
+    (< (absolute (- (square guess) x)) ; boolean result, such that the absolute value of guess*guess - x is less than tolerance
+        tolerance))
+
+(defun try (guess x) ; two formal params of integers
+    (if (tolerable guess x) ; if the result of tolerable on guess & x is true, return guess, else, invoke try on the result of improve & guess and x
+        guess
+        (try (improve guess x) x))) ; else, invoke improve on (guess & x) then invoke try on the result of improve and x
+
+(defun sqroot (x)
+    (try 1 x))
+
+(write (sqroot 25.0))
+
---
+++ sqroot/heron.scm
@@ -0,0 +1,29 @@
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (square x)
+    (* x x))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (absolute x)
+    (if (< x 0)
+        (- x)
+           x))
+
+(define (good-enough? guess x)
+    (< (absolute (- (square guess) x))
+        .001))
+
+
+(define (try guess x)
+    (if (good-enough? guess x)
+        guess
+        (try (improve guess x) x)))
+
+(define (sqroot x) (try 1 x))
+
+(write (sqroot 25.0))
+
+
