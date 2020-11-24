e2dd8184dccde3a5763c0f08b75e2e2cddedcc18
elsoleiro <elsoleiro@gmail.com>
2020-11-24 19:45:12 +0000

    exercise 1.3

--- Chapter 1/exercises.scm
+++ Chapter 1/exercises.scm
@@ -10,4 +10,21 @@
        (- 6.0 2.0)
        (- 2.0 7.0))))
 
-; 1.3 
+; 1.3 -- revisit this with some (more) original thought
+(display "\n")
+
+(define square
+   (lambda (a b c)
+      (+ (* a a) (* b b) (* c c))))
+
+(define (smallest a b c)
+   (cond ((and (<= a b) (<= a c)) (* a a))
+         ((and (<= b a) (<= b c)) (* b b))
+         ((and (<= c a) (<= c b)) (* c c))))
+(define (answer a b c)
+   (- (square a b c)
+      (smallest a b c)))
+
+
+
+
